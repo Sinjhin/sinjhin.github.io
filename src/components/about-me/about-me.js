e8d1daa8023a1666f4a0ddd0d02b56faa1771d06
John Gardner <john@ardea.io>
2018-03-19 15:17:05 -0500

    Filled out the rest of the about-me sections

--- src/components/about-me/about-me.js
+++ src/components/about-me/about-me.js
@@ -4,8 +4,45 @@ import './about-me.less';
 import view from './about-me.stache';
 
 export const ViewModel = DefineMap.extend({
-  message: {
-    default: 'This is the sin-about-me component'
+  description: {
+    type: 'string',
+    default: ''
+  },
+  aboutMeDescriptionImage: {
+    type: 'string',
+    default: '/src/img/about-me/telekinetic-laptop.jpg'
+  },
+  setDescription(item) {
+    switch(item) {
+      case 'travel':
+        this.description = 'travel';
+        this.aboutMeDescriptionImage = '/src/img/about-me/mountain-top-buddha.jpg';
+        break;
+      case 'motorcycles':
+        this.description = 'motorcycles';
+        this.aboutMeDescriptionImage = '/src/img/about-me/bike-and-lake.jpg';
+        break;
+      case 'gaming':
+        this.description = 'gaming';
+        this.aboutMeDescriptionImage = '/src/img/about-me/gaming-setup.jpg';
+        break;
+      case 'airsoft':
+        this.description = 'airsoft';
+        this.aboutMeDescriptionImage = '/src/img/about-me/airsoft-group.jpg';
+        break;
+      case 'reading':
+        this.description = 'reading';
+        this.aboutMeDescriptionImage = '/src/img/about-me/reading.jpg';
+        break;
+      case 'dog':
+        this.description = 'dog';
+        this.aboutMeDescriptionImage = '/src/img/about-me/cool-dog.JPG';
+        break;
+      default:
+        this.description = '';
+        this.aboutMeDescriptionImage = '/src/img/about-me/telekinetic-laptop.jpg';
+        break;
+    }
   }
 });
 
